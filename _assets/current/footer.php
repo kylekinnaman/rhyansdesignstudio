da6aae9e16a67cea72a03b21a16887b2a80e2534
supakow <kyle.kinnaman@gmail.com>
2014-05-13 23:07:54 +0000

    removing old site theme from _assets, and OSX files

--- _assets/current/footer.php
+++
@@ -1,6 +0,0 @@
-		<div id="footer">
-			<p><a href="/category/bracelets">Bracelets</a><a href="/category/bridal">Bridal</a><a href="/category/colored-stones">Colored Stones</a><a href="/category/custom">Custom</a><a href="/category/diamond">Diamond</a><a href="/category/earrings">Earrings</a><a href="/category/pearls">Pearls</a><a href="/category/pendants">Pendants</a><a href="/category/rings">Rings</a></p>
-			<p><a href="http://maps.google.com/maps?f=q&amp;hl=en&amp;geocode=&amp;q=rhyans+design+studio&amp;sll=33.976753,-84.478549&amp;sspn=0.001791,0.002414&amp;ie=UTF8&amp;ll=33.977191,-84.478549&amp;spn=0.001791,0.002414&amp;t=h&amp;z=19&amp;iwloc=A" title="Map to our showroom">1050 East Piedmont Rd. Suite B, Marietta, GA 30062</a> | Phone: 678.560.5718‎ | <a href="/contact">Contact Us</a></p>
-		</div>
-	</body>
-</html>
